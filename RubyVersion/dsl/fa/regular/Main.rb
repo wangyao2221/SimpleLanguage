aafc050ebd2740e220880c779f145f581396967f
wangyao2221 <wangyao2221@163.com>
2019-12-01 23:12:03 +0800

    pattern语法解析器 test

--- RubyVersion/dsl/fa/regular/Main.rb
+++ RubyVersion/dsl/fa/regular/Main.rb
@@ -3,17 +3,20 @@ $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'Pattern'
 require 'treetop'
 
-pattern = Repeat.new(
-    Choose.new(
-              Concatenate.new(Literal.new('a'), Literal.new('b')),
-              Literal.new('a')
-    )
-)
-
-puts pattern
-puts pattern.matchs?('aaab')
+#pattern = Repeat.new(
+#    Choose.new(
+#              Concatenate.new(Literal.new('a'), Literal.new('b')),
+#              Literal.new('a')
+#    )
+#)
+#
+#puts pattern
+#puts pattern.matchs?('aaab')
 
 Treetop.load'pattern'
 parser = PatternParser.new
 parse_tree = parser.parse('(a(|b))*')
 puts parse_tree.inspect
+pattern = parse_tree.to_ast
+puts pattern.matchs?('abaab')
+puts pattern.matchs?('abba')
