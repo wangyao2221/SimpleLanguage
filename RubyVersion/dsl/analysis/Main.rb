2443134d847c6663e9748a0dd1621c7bbe54e811
wangyao2221 <wangyao2221@163.com>
2019-12-21 23:59:44 +0800

    LexicalAnalyzer

--- RubyVersion/dsl/analysis/Main.rb
+++ RubyVersion/dsl/analysis/Main.rb
@@ -2,3 +2,7 @@ $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'LexicalAnalyzer'
 
 puts LexicalAnalyzer.new('y = x * 7').analyze
+puts
+puts LexicalAnalyzer.new('while (x < 5) { x = x * 3 }').analyze
+puts
+puts LexicalAnalyzer.new('if (x < 10) { y = true; x = 0 } else { do-nothing }').analyze
